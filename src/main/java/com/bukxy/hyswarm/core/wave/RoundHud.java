4e4f4283047daf1fca93aa336d4fec888463e5c6
bukxy <warco16@gmail.com>
2026-04-15 20:50:31 +0200

    feat: Add RoundHud Base

---
+++ src/main/java/com/bukxy/hyswarm/core/wave/RoundHud.java
@@ -0,0 +1,27 @@
+package com.bukxy.hyswarm.core.wave;
+
+import com.hypixel.hytale.server.core.Message;
+import com.hypixel.hytale.server.core.entity.entities.player.hud.CustomUIHud;
+import com.hypixel.hytale.server.core.ui.builder.UICommandBuilder;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import org.jspecify.annotations.NonNull;
+
+public class RoundHud extends CustomUIHud {
+
+	RoundComponent roundComponent;
+
+	public RoundHud(@NonNull PlayerRef playerRef, RoundComponent roundComponent) {
+		super(playerRef);
+		this.roundComponent = roundComponent;
+	}
+
+	@Override
+	protected void build(@NonNull UICommandBuilder uiCommandBuilder) {
+		uiCommandBuilder.append("Hud/RoundInfos.ui");
+
+		uiCommandBuilder.set("#Timer.TextSpans", Message.raw("Timer : " + String.valueOf(roundComponent.getRoundTimer())));
+		uiCommandBuilder.set("#TotalKills.TextSpans", Message.raw("Total Kills : " + String.valueOf(roundComponent.getTotalKills())));
+		uiCommandBuilder.set("#StageNumber.TextSpans", Message.raw("Stage : " + String.valueOf(roundComponent.getStageNumber())));
+
+	}
+}
